5afff32797b774c4eee6fcc4359178159b4cdd60
James brink <brink.james@gmail.com>
2014-05-08 17:47:14 -0700

    Added a root object to JSON response to match XML

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -2,12 +2,10 @@ module RequestHelper
   # If no accept header is supplied, default response is JSON
   def response
     case last_request.content_type
-      when 'application/json'
-        JSON.parse(last_response.body)
       when 'application/xml', 'text/xml'
         Hash.from_xml(last_response.body)['barcode']
       else
-        JSON.parse(last_response.body)
+        JSON.parse(last_response.body)['barcode']
     end
   end
 end
